7e8121b018cc7e176ac1f6eb2592dc568fdf292f
harold <hehurtado9@misena.edu.co>
2020-04-23 08:43:06 -0500

    6 cambios

--- clases/Articulos.php
+++ clases/Articulos.php
@@ -1,7 +1,5 @@
 <?php
  class articulos{
-
-    // esta funcion es para agregar la imagen en la tabla imagenes
 public function agregaImagen($datos){
 $c=new conectar();
 
@@ -27,39 +25,7 @@ $resultado = mysqli_query($conexion, $sql);
 
 }
 
-public function insertaArticulo($datos)
-{
-
-
-    $fecha =date('Y-m-d');
-    $c=new conectar();
-
-    $conexion=$c->conexion();
-
-    $sql="INSERT into articulos (id_categoria,
-    id_imagen,
-    id_usuario,
-    nombre,
-    descripcion,
-    cantidad,
-    precio,
-    fechaCaptura) 
-values ('$datos[0]',
-'$datos[1]',
-'$datos[2]',
-'$datos[3]',
-'$datos[4]',
-'$datos[5]',
-'$datos[6]',
-'$fecha')";
-
-$resultado = mysqli_query($conexion, $sql);
-		if (!$resultado) {
-			return mysqli_error($conexion);
-		} else {
-			return mysqli_insert_id($conexion);
-		}
-}
-
 
 }
+
+?>
